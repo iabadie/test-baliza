84fa872d6afa132b3a7553eb1be560efcc8f82cc
Nacho <ignacioabadie@live.com.ar>
2018-11-06 20:30:57 -0300

    Kickoff

---
+++ src/main/java/ar/edu/untref/dyasc/Program.java
@@ -0,0 +1,8 @@
+package ar.edu.untref.dyasc;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Plane World");
+    }
+}
---
+++ src/test/java/ar/edu/untref/dyasc/TestProgram.java
@@ -0,0 +1,14 @@
+package ar.edu.untref.dyasc;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
+public class TestProgram {
+
+    @Test
+    public void testQuePasa() {
+      Assert.assertEquals(true, true);
+    }
+}
